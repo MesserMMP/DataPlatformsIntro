b663b075d236cbee14deb692792209a8030d5043
Mikhail <46933536+MesserMMP@users.noreply.github.com>
2025-04-24 21:08:19 +0300

    Update 2_download_data.sh

--- hw4/scripts/2_download_data.sh
+++ hw4/scripts/2_download_data.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
 
+sudo -i -u hadoop bash <<EOF
+hdfs dfs -mkdir -p /input
+wget -nc https://raw.githubusercontent.com/MesserMMP/Datasets/main/Electric_Vehicle_Population_Data.csv -O electric_vehicles.csv
+hdfs dfs -put -f electric_vehicles.csv /input
+EOF
+
 echo "✅ Данные загружены в HDFS"
