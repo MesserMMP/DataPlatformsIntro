aac87c041af7b9da37f71c77697d1ab0995877f4
Mikhail <46933536+MesserMMP@users.noreply.github.com>
2025-04-24 21:08:33 +0300

    Update 3_start_metastore.sh

--- hw4/scripts/3_start_metastore.sh
+++ hw4/scripts/3_start_metastore.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+sudo -i -u hadoop bash <<EOF
+nohup hive --hiveconf hive.server2.enable.doAs=false \
+     --hiveconf hive.security.authorization.enabled=false \
+     --service metastore > /tmp/metastore.log 2>&1 &
+EOF
 
 echo "✅ Hive Metastore запущен"
